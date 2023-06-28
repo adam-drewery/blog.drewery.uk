803e2ac588e5b9ef14c617293021fc6b7dc1e0cc
Adam Drewery <adam@drewery.uk>
2023-06-29 01:39:25 +0200

    fix menu init code

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -1,27 +1,13 @@
-(function($) {
+window.initializeMenu = () => {
+    //metis menu
+    $("#side-menu").metisMenu();
 
-    'use strict';
-
-    function initMetisMenu() {
-        //metis menu
-        $("#side-menu").metisMenu();
-    }
-
-    function initLeftMenuCollapse() {
-        // Left menu collapse
-        $('.button-menu-mobile').on('click', function (event) {
-            event.preventDefault();
-            $("body").toggleClass("enlarged");
-        });
-    }
-
-    function init() {
-        initMetisMenu();
-        initLeftMenuCollapse();
-    }
-    init();
-
-})(jQuery)
+    // Left menu collapse
+    $('.button-menu-mobile').on('click', function (event) {
+        event.preventDefault();
+        $("body").toggleClass("enlarged");
+    });
+}
 
 window.highlightCode = () => { hljs.highlightAll(); };
 
