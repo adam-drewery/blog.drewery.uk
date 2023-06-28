bf4a5b393c8861a463d44d16f0c466815aaadb65
Adam Drewery <adam@drewery.uk>
2023-06-28 12:50:39 +0200

    add blazor project

---
+++ SiteContext.cs
@@ -0,0 +1,37 @@
+using System.Net.Http.Json;
+
+namespace blog;
+
+internal class SiteContext
+{
+    public const string GitHub = "https://github.com/adam-drewery";
+    public const string StackOverflow = "https://stackoverflow.com/users/1228263/adam-drewery";
+    public const string LinkedIn = "https://www.linkedin.com/in/adam-drewery-57678792/";
+    
+    public IList<PostDetails> Posts { get; } = new List<PostDetails>();
+    
+    public static async Task<SiteContext> InitializeAsync(HttpClient http)
+    {
+        var siteContext = new SiteContext();
+        var posts = await http.GetFromJsonAsync<IDictionary<string, PostDetails>>("code/index.json");
+        
+        foreach (var post in posts)
+        {
+            post.Value.Id = post.Key;
+            siteContext.Posts.Add(post.Value);
+        }
+
+        return siteContext;
+    }
+}
+
+internal class PostDetails
+{
+    public string Id { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public string Date { get; set; } = null!;
+
+    public string[] Tags { get; set; } = Array.Empty<string>();
+}
