714cfdffe148a878709facbbf936ed452bde28cb
Adam Drewery <adam@drewery.uk>
2023-06-29 22:26:29 +0200

    whoops copypaste error

--- rss/generate-rss.js
+++ rss/generate-rss.js
@@ -23,7 +23,7 @@ for (const articleId in index) {
     
     feed.item({
         title:  article.title,
-        description: marked(data),
+        description: marked(markdown),
         url: `https://blog.drewery.uk/post/${articleId}`,
         date: article.date
     });
