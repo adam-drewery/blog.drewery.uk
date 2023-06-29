07c34e3ae18620c8019d8e21ef4cab0c8d608ef1
Adam Drewery <adam@drewery.uk>
2023-06-29 20:50:20 +0200

    Generate RSS maybe.

---
+++ rss/generate-rss.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const RSS = require('rss');
+
+// Read index.json
+const index = JSON.parse(fs.readFileSync('content/index.json', 'utf8'));
+
+// Create a feed
+const feed = new RSS({
+    title: 'Adam Drewery\'s Blog',
+    description: '',
+    feed_url: 'https://raw.githubusercontent.com/adam-drewery/blog/main/rss.xml',
+    site_url: 'blog.drewery.uk',
+    managingEditor: 'Adam Drewery,
+    webMaster: 'Adam Drewery',
+    language: 'en',
+    pubDate: new Date().toUTCString(),
+});
+
+// Loop over the articles in the index and add them to the feed
+for (const articleId in index) {
+    const article = index[articleId];
+    feed.item({
+        title:  article.title,
+        description: 'You can add a description here',
+        url: `https://yourwebsite.com/${articleId}`, // URL of the article
+        date: article.date,
+    });
+}
+
+// Write the XML to a file
+fs.writeFileSync('rss.xml', feed.xml());
